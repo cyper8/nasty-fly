1089d21072df4704cc89821c734bc09ffab64217
Anton Yashchuk <anton.yashchuk@gmail.com>
2022-03-25 16:07:13 +0200

    build toolchain upd

---
+++ rollup.config.js
@@ -0,0 +1,68 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+// import babel from '@rollup/plugin-babel';
+import html from '@web/rollup-plugin-html';
+import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
+import { terser } from 'rollup-plugin-terser';
+import { copy } from '@web/rollup-plugin-copy';
+
+export default {
+  input: 'index.html',
+  output: {
+    entryFileNames: '[hash].js',
+    chunkFileNames: '[hash].js',
+    assetFileNames: '[hash][extname]',
+    format: 'es',
+    dir: 'docs',
+  },
+  preserveEntrySignatures: false,
+
+  plugins: [
+    /** Enable using HTML as rollup entrypoint */
+    html({minify: true}),
+    /** Resolve bare module imports */
+    nodeResolve(),
+    /** Minify JS */
+    terser(),
+    /** Bundle assets references via import.meta.url */
+    importMetaAssets(),
+    copy({patterns: ['assets', 'README.md']}),
+    // /** Compile JS to a lower language target */
+    // babel({
+    //   babelHelpers: 'bundled',
+    //   presets: [
+    //     [
+    //       require.resolve('@babel/preset-env'),
+    //       {
+    //         targets: [
+    //           'last 3 Chrome major versions',
+    //           'last 3 Firefox major versions',
+    //           'last 3 Edge major versions',
+    //           'last 3 Safari major versions',
+    //         ],
+    //         modules: false,
+    //         bugfixes: true,
+    //       },
+    //     ],
+    //   ],
+    //   plugins: [
+    //     [
+    //       require.resolve('babel-plugin-template-html-minifier'),
+    //       {
+    //         modules: {
+    //           lit: ['html', 'LitElement']
+    //         },
+    //         failOnError: false,
+    //         strictCSS: true,
+    //         htmlMinifier: {
+    //           collapseWhitespace: true,
+    //           conservativeCollapse: true,
+    //           removeComments: true,
+    //           caseSensitive: true,
+    //           minifyCSS: true,
+    //         },
+    //       },
+    //     ],
+    //   ],
+    // }),
+  ],
+};
